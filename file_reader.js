22b7312d10f67273d98051da802c9875e6903fee
Yi-en Tsai <m951235@gmail.com>
2024-07-23 18:19:09 -0700

    Update

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,17 @@
+
 const fs = require("fs");
 
-fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+fs.readFile("./firstname.txt", "utf-8", (err, firstname) => {
+  fs.readFile("./lastname.txt", "utf-8", (err, lastname) => {
+    fs.readFile("./age.txt", "utf-8", (err, age) => {
+      fs.readFile("./hobbies.txt", "utf-8", (err, hobby) => {
+        let hobbyArr = hobby.replace(',','').replace(' ','').split('"') //del "," & space then convert to array
+        hobbyArr.shift() //del: "["
+        hobbyArr.pop() // del: "]"
+        let hobbies = hobbyArr.filter(n => n) //del: "" empty element
+
+        console.log(`${firstname} ${lastname} is ${age} years old and his hoobies are ${hobbies[0]} and ${hobbies[1]}.`)
+      })
+    })
+  })
 })
